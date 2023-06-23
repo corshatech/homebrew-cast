4a59ea06c0525934cd0578eece8f535adf9c0331
Matthew Clive <max@corsha.com>
2023-06-23 15:20:10 -0400

    Fix linting

--- Formula/cast.rb
+++ Formula/cast.rb
@@ -1,19 +1,19 @@
 class Cast < Formula
-    desc "Corsha API Assessment Tool"
-    homepage "https://github.com/corshatech/cast"
-    url "https://github.com/corshatech/cast.git",
-        tag:        "v0.1.0-rc.10",
-        revision:   "c76ffc55c5c212785cc28e88d980370d3e0e4896"
-    license "Apache-2.0"
-    head "https://github.com/corshatech/cast.git", branch: "main"
+  desc "Corsha API Assessment Tool"
+  homepage "https://github.com/corshatech/cast"
+  url "https://github.com/corshatech/cast.git",
+    tag:      "v0.1.0-rc.10",
+    revision: "c76ffc55c5c212785cc28e88d980370d3e0e4896"
+  license "Apache-2.0"
+  head "https://github.com/corshatech/cast.git", branch: "main"
 
-    depends_on "go" => :build
+  depends_on "go" => :build
 
-    def install
-        # Don't dirty the git tree
-        rm_rf ".brew_home"
+  def install
+    # Don't dirty the git tree
+    rm_rf ".brew_home"
 
-        system "make", "cast"
-        bin.install "build/package/cast"
-    end
+    system "make", "cast"
+    bin.install "build/package/cast"
+  end
 end
